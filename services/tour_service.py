013538690266cfc16a0ae9508998a132e311f64e
juraldinho <d.otabek90@gmail.com>
2026-03-10 15:27:18 +0500

    Implement function to get current month's tours
    Added a new function to retrieve tours for the current month.

--- services/tour_service.py
+++ services/tour_service.py
@@ -1,6 +1,7 @@
 from database.queries import create_tour
 from services.date_parser import parse_single_date
-
+from datetime import datetime
+from database.queries import get_tours_for_month
 
 def save_tour(
     user_id: int,
@@ -21,3 +22,19 @@ def save_tour(
         status=status.strip(),
         income=income,
     )
+
+def get_current_month_tours(user_id: int):
+    now = datetime.now()
+
+    month_start = now.replace(day=1).strftime("%Y-%m-%d")
+
+    if now.month == 12:
+        next_month = now.replace(year=now.year + 1, month=1, day=1)
+    else:
+        next_month = now.replace(month=now.month + 1, day=1)
+
+    month_end = (next_month).strftime("%Y-%m-%d")
+
+    tours = get_tours_for_month(user_id, month_start, month_end)
+
+    return tours
